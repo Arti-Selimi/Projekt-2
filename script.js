d7dca78faa0d1de509949da9aa3304989067389f
mozArti <selimiarti@outlook.com>
2023-04-11 18:29:46 +0200

    gattiii u krie

--- script.js
+++ script.js
@@ -1,105 +1,25 @@
 let inputs = document.querySelectorAll('.guessingLetters')
 let firstGuess = document.querySelectorAll('.first-row')
-let secondGuess = document.querySelectorAll('.second-row')
-let thirdGuess = document.querySelectorAll('.third-row')
-let fourthGuess = document.querySelectorAll('.fourth-row')
-let fifthGuess = document.querySelectorAll('.fifth-row')
 let checkBtn = document.getElementById("checkBtn")
 let answer = document.getElementById('answer')
+const arrayOfInputs = [document.querySelectorAll('.first-row'), document.querySelectorAll('.second-row'), document.querySelectorAll('.third-row'), document.querySelectorAll('.fourth-row'), document.querySelectorAll('.fifth-row')]
+const totalWords = JSON.parse(words)
+const dailyWordObj = totalWords[Math.floor(Math.random()*2314)]
+const dailyWord = Array.from(dailyWordObj.word)
+console.log(dailyWord)
+let counter = 0
 
-let dailyWord = ['W', 'O', 'R', 'D', 'S']
-
-let checkGuess = (guess)=>{
-    if(guess === 'first') {
-        for(let j = 0; j < dailyWord.length; j++) {
-            for(let i = 0; i < firstGuess.length; i++){
-                if(firstGuess[i].value.toUpperCase() === dailyWord[i]){
-                    firstGuess[i].style.backgroundColor = "salmon"
-                } else {
-                    firstGuess[i].style.backgroundColor = "gray"
-                }
-                firstGuess[i].readOnly = true
-                if (firstGuess[i].style.backgroundColor === "salmon") {
-                    answer.innerHTML = "Good job! Got it on the first try."
-                } else {
-                    answer.innerHTML = "Not quite."
-                }
+let checkGuess = ()=>{
+    console.log('counter',counter)
+    arrayOfInputs[counter].forEach((e, i) => {
+            if (e.value.toUpperCase() === dailyWord[i]) {
+                e.style.background = "green"
+            } else if (dailyWord.includes(e.value.toUpperCase())) {
+                e.style.background = "#ffcd91"
+            } else {
+                e.style.background = "gray" 
             }
         }
-        checkBtn.setAttribute('onclick', 'checkGuess(`second`)')
-    } else if (guess === 'second') {
-        for(let j = 0; j < dailyWord.length; j++) {
-            for(let i = 0; i < secondGuess.length; i++){
-                if(secondGuess[i].value.toUpperCase() === dailyWord[i]) {
-                    secondGuess[i].style.backgroundColor = "salmon"
-                } else {
-                    secondGuess[i].style.backgroundColor = "gray"
-                }
-                secondGuess[i].readOnly = true
-                if (secondGuess[i].style.backgroundColor === "salmon") {
-                    answer.innerHTML = "Good job! Got it on the second try."
-                } else {
-                    answer.innerHTML = "Not quite."
-                }
-            }
-        }
-        checkBtn.setAttribute('onclick', 'checkGuess(`third`)')
-    } else if (guess === 'third') {
-        for(let j = 0; j < dailyWord.length; j++) {
-            for(let i = 0; i < thirdGuess.length; i++){
-                if(thirdGuess[i].value.toUpperCase() === dailyWord[i]) {
-                    thirdGuess[i].style.backgroundColor = "salmon"
-                } else {
-                    thirdGuess[i].style.backgroundColor = "gray"
-                }
-                thirdGuess[i].readOnly = true
-                if (thirdGuess[i].style.backgroundColor === "salmon") {
-                    answer.innerHTML = "Good job! Got it on the third try."
-                } else {
-                    answer.innerHTML = "Not quite."
-                }
-            }
-        }
-        checkBtn.setAttribute('onclick', 'checkGuess(`fourth`)')
-    } else if (guess === 'fourth') {
-        for(let j = 0; j < dailyWord.length; j++) {
-            for(let i = 0; i < fourthGuess.length; i++){
-                if(fourthGuess[i].value.toUpperCase() === dailyWord[i]) {
-                    fourthGuess[i].style.backgroundColor = "salmon"
-                } else {
-                    fourthGuess[i].style.backgroundColor = "gray"
-                }
-                fourthGuess[i].readOnly = true
-                if (fourthGuess[i].style.backgroundColor === "salmon") {
-                    answer.innerHTML = "Good job! Got it on the fourth try."
-                } else {
-                    answer.innerHTML = "Not quite."
-                }
-            }
-        }
-        checkBtn.setAttribute('onclick', 'checkGuess(`fifth`)')
-    } else {
-        for(let j = 0; j < dailyWord.length; j++) {
-            for(let i = 0; i < fifthGuess.length; i++){
-                if(fifthGuess[i].value.toUpperCase() === dailyWord[i]) {
-                    fifthGuess[i].style.backgroundColor = "salmon"
-                } else {
-                    fifthGuess[i].style.backgroundColor = "gray"
-                }
-                fifthGuess[i].readOnly = true
-                if (fifthGuess[i].style.backgroundColor === "salmon") {
-                    answer.innerHTML = "Good job! Got it on the fifth try."
-                } else {
-                    answer.innerHTML = "Not quite."
-                }
-            }
-        }
-    }
-    for(let i = 0; i < inputs.length; i++) {
-        if (answer.innerHTML !== "Not quite.") {
-            inputs[i].readOnly = true
-        } else {
-            console.log('o')
-        }
-    }
-}
+    )
+    counter++
+};
