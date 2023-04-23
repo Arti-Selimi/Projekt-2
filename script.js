d1325166ff5fd000d809eb6dbd3012318aba6961
mozArti <selimiarti@outlook.com>
2023-04-23 19:49:00 +0200

    Finished project

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let inputs = document.querySelectorAll('.guessingLetters')
-let firstGuess = document.querySelectorAll('.first-row')
 let checkBtn = document.getElementById("checkBtn")
 let answer = document.getElementById('answer')
 const arrayOfInputs = [document.querySelectorAll('.first-row'), document.querySelectorAll('.second-row'), document.querySelectorAll('.third-row'), document.querySelectorAll('.fourth-row'), document.querySelectorAll('.fifth-row')]
@@ -12,13 +11,14 @@ let counter = 0
 let checkGuess = ()=>{
     console.log('counter',counter)
     arrayOfInputs[counter].forEach((e, i) => {
-            if (e.value.toUpperCase() === dailyWord[i]) {
-                e.style.background = "green"
-            } else if (dailyWord.includes(e.value.toUpperCase())) {
-                e.style.background = "#ffcd91"
-            } else {
-                e.style.background = "gray" 
-            }
+                if (e.value.toUpperCase() === dailyWord[i]) {
+                    e.style.background = "green"
+                } else if (dailyWord.includes(e.value.toUpperCase())) {
+                    e.style.background = "#ffcd91"
+                } else {
+                    e.style.background = "gray" 
+                }
+                e.setAttribute('readonly', true)
         }
     )
     counter++
